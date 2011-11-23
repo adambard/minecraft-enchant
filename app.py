7ac6b9b4a3e4a5c0811fb737e2ed1bb2b9b3846a
Adam Bard <github@adambard.com>
2011-11-22 23:02:50 -0800

    Updated app.py

--- app.py
+++ app.py
@@ -151,4 +151,4 @@ def armor():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get('PORT', 5000))
-    app.run(host=0.0.0.0, port=port)
+    app.run(host='0.0.0.0', port=port)
